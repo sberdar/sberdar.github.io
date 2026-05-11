edff5c490df38c505f353004caf76d0dc4e345c4
sberdar <stas.berdar97@gmail.com>
2026-05-11 11:01:25 -0700

    added reading estimate, sigma rule repo link.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
-
+  eleventyConfig.addPassthroughCopy("CNAME");
+  
+  eleventyConfig.addFilter("readingTime", function(content) {
+    const wordsPerMinute = 200;
+    const words = content.trim().split(/\s+/).length;
+    const minutes = Math.ceil(words / wordsPerMinute);
+    return `${minutes} min read`;
+  });
   return {
     dir: {
       input: ".",
